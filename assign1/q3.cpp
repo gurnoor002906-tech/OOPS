5efe5f9dd2178bd6b9e1335448b40b69e1276c96
Gurnoor Kaur <gurnoor002906@gmail.com>
2026-01-19 18:29:07 +0530

    q3.cpp

---
+++ assign1/q3.cpp
@@ -0,0 +1,16 @@
+//3. Write a program to add, subtract, multiply, and divide two numbers.
+#include<iostream>
+using namespace std ;
+int main(){
+ int a, b;
+ cout<<"enter two numbers"<<endl;
+ cin>>a>>b;
+ int add=a+b;
+ int subract=a-b;
+ int multiply=a*b;
+ float divide=(float)a/b;
+ cout<<"ADD :"<<add<<endl;
+ cout<<"SUBRACT :"<<subract<<endl;
+ cout<<"MULTIPLY :"<<multiply<<endl;
+ cout<<"DIVIDE :"<<divide<<endl;
+}

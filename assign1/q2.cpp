8524dedcf1f29adb985f1a3deef141f09f6a9fdb
Gurnoor Kaur <gurnoor002906@gmail.com>
2026-01-19 18:27:49 +0530

    q2.cpp

---
+++ assign1/q2.cpp
@@ -0,0 +1,15 @@
+//2.WAP to input an integer, decimal and character and display it.
+#include<iostream>
+using namespace std ;
+int main(){
+ int a; 
+ float b;
+ char c;
+ cout<<"INPUT INTEGER"<<endl;
+ cin>>a;
+ cout<<"INPUT DECIMAL"<<endl;
+ cin>>b;
+ cout<<"INPUT CHARACTER"<<endl;
+ cin>>c;
+ cout<<"Integer "<<a<<", DECIMAL "<<b<<" , CHARACTER "<<c<<endl;
+}

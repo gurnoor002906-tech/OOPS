abaee840fabd5b2d3e7cc63aee6c750fa72f137a
Gurnoor Kaur <gurnoor002906@gmail.com>
2026-05-05 22:05:39 +0530

    Create 4.cpp

---
+++ assign6/4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class B;
+
+class A{
+private:
+    int x;
+
+public:
+    A(int a){
+        x = a;
+    }
+
+    friend int add(A a, B b);
+};
+
+class B{
+private:
+    int y;
+
+public:
+    B(int b){
+        y = b;
+    }
+
+    friend int add(A a, B b);
+};
+
+int add(A a, B b){
+    return a.x + b.y;
+}
+
+int main(){
+    A a1(10);
+    B b1(20);
+
+    cout << "Sum = " << add(a1, b1);
+    return 0;
+}
